03bedccf1df16ad20bc8372e4873cd51471ec853
Marcos Chesniuk <jmdchesniuk@gmail.com>
2022-03-19 03:10:25 -0300

    WIP
    Need to change client for pool connection

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,19 @@
+if (process.env.NODE_ENV === 'development') {
+  require('dotenv').config({ path: __dirname+'/../.env' });
+}
+
 import express from 'express';
 
 const db = require('./queries');
 
 const app = express();
-//const port = 3000;
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = '3000';
-}
+
+console.log(process.env.NODE_ENV);
+console.log(process.env.DATABASE_URL);
+console.log(process.env.LALO);
+
+
+let port = process.env.PORT || '3000';
 
 app.get('/', (req, res) => {
   res.send('Test OK!');
@@ -16,5 +22,6 @@ app.get('/', (req, res) => {
 app.get('/clients', db.getClients);
 
 app.listen(port, () => {
+  
   return console.log(`Server is listening on port ${port}`);
 });
--- src/queries.ts
+++ src/queries.ts
@@ -1,20 +1,32 @@
-const Pool = require('pg').Pool;
+const { Client } = require('pg');
 
-const pool = new Pool({
+const ssl = {
+  rejectUnauthorized: false
+}
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL_LOCAL,
+  ssl: process.env.NODE_ENV === 'development' ? false : ssl
+});
+
+
+/* const pool = new Pool({
   user: 'vank',
   host: 'localhost',
   database: 'vank',
   password: 'vank',
   port: 5432,
-});
+}); */
 
 const getClients = (req, resp) => {
-  pool.query('SELECT * FROM clients ORDER BY ID ASC', (err, res) => {
+  client.connect();
+  client.query('SELECT * FROM clients ORDER BY ID ASC', (err, res) => {
     if (err) {
       throw err;
     }
 
     resp.status(200).json(res.rows);
+    client.end();
   });
 };
 
