14de90ef214d2e0b98e3c2945d0cadb06dfc5c77
Marcos Chesniuk <jmdchesniuk@gmail.com>
2022-03-19 13:53:32 -0300

    WIP

--- src/queries.ts
+++ src/queries.ts
@@ -1,15 +1,17 @@
 const { Client, Pool } = require('pg');
 
-// const ssl = {
-//   rejectUnauthorized: false
-// }
+const ssl = {
+  rejectUnauthorized: false
+}
 
 // const client = new Client({
 //   connectionString: process.env.DATABASE_URL_LOCAL,
 //   ssl: process.env.NODE_ENV === 'development' ? false : ssl
 // });
 
-const pool = new Pool();
+const pool = new Pool({
+  ssl: process.env.NODE_ENV === 'development' ? false : ssl
+});
 
 const query = (text, params, callback) => {
   return pool.query(text, params, callback)
