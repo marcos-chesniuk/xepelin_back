abcc2388b917dfdfc20b5a79da5e3e135cc11694
Marcos Chesniuk <jmdchesniuk@gmail.com>
2022-03-19 12:46:58 -0300

    WIP

--- src/queries.ts
+++ src/queries.ts
@@ -1,32 +1,29 @@
-const { Client } = require('pg');
+const { Client, Pool } = require('pg');
 
-const ssl = {
-  rejectUnauthorized: false
-}
+// const ssl = {
+//   rejectUnauthorized: false
+// }
 
-const client = new Client({
-  connectionString: process.env.DATABASE_URL_LOCAL,
-  ssl: process.env.NODE_ENV === 'development' ? false : ssl
-});
+// const client = new Client({
+//   connectionString: process.env.DATABASE_URL_LOCAL,
+//   ssl: process.env.NODE_ENV === 'development' ? false : ssl
+// });
 
+const pool = new Pool();
 
-/* const pool = new Pool({
-  user: 'vank',
-  host: 'localhost',
-  database: 'vank',
-  password: 'vank',
-  port: 5432,
-}); */
+const query = (text, params, callback) => {
+  return pool.query(text, params, callback)
+};
 
 const getClients = (req, resp) => {
-  client.connect();
-  client.query('SELECT * FROM clients ORDER BY ID ASC', (err, res) => {
+  pool.connect();
+  pool.query('SELECT * FROM clients ORDER BY ID ASC', (err, res) => {
     if (err) {
       throw err;
     }
 
     resp.status(200).json(res.rows);
-    client.end();
+    pool.end();
   });
 };
 
