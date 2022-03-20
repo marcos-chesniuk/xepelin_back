8247a06f020693bded1e178e271c7540d4a61f6d
Marcos Chesniuk <jmdchesniuk@gmail.com>
2022-03-19 23:26:11 -0300

    Create client endpoint

--- src/routes/clients.ts
+++ src/routes/clients.ts
@@ -15,3 +15,35 @@ router.get('/list', async (req: any, res: Response) => {
     const { rows } = await db.query('SELECT * FROM clients ORDER BY ID ASC');
     res.status(200).send(rows);
 });
+
+router.post('/create', jsonParser, async (req: TypedRequestBody<ClientCreateBody>, res: Response) => {
+    const params = req.body;
+
+    let status = 200;
+    let message;
+
+    try {
+        await db.query(`
+            INSERT INTO clients (internal_id, company_name, tax_id, currency_id, monthly_quota, bank_registry)
+                VALUES ($1, $2, $3, $4, $5, $6)`,
+            [ 
+                params.internalId,
+                params.companyName,
+                params.taxId,
+                params.currencyId,
+                params.monthlyQuota,
+                params.bankRegistry
+            ]
+        );
+        status = 200;
+        message = OK_RESPONSE;
+
+    } catch (error) {
+        status = 500;
+        message = ERROR_RESPONSE(error.detail);
+        
+    } finally {
+        res.status(status).send(message)
+    };
+
+});
