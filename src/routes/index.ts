e2e5f9153bf79c32d7a7d311cee073bbfc1f04e3
Marcos Chesniuk <jmdchesniuk@gmail.com>
2022-03-19 23:13:43 -0300

    Some constants and interfaces

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,8 @@
+import { Response } from "express";
 const clients = require('./clients');
 
 module.exports = app => {
-    app.get('/', (req, res) => {
+    app.get('/', (req: any, res: Response) => {
         res.send('Test OK!');
     });
     
