f3902548b56698ac73bafdadf9696dd5fe4fe2a7
Marcos Chesniuk <jmdchesniuk@gmail.com>
2022-03-23 02:43:10 -0300

    DONE:: Currency convertion inside endpoint

--- src/routes/invoices.ts
+++ src/routes/invoices.ts
@@ -1,22 +1,21 @@
-import { TypedRequestBody, ClientCreateBody } from "../utils/interfaces/request";
+// import { TypedRequestBody, ClientCreateBody } from "../utils/interfaces/request";
+import { DbInvoice } from "../utils/interfaces/invoices";
 import { Response } from "express";
 const { OK_RESPONSE, ERROR_RESPONSE } = require("../utils/responses");
-// const db = require('../db/connection');
+const axios = require('axios');
 
 const Router = require('express-promise-router');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const db = require('../db/queries');
 
 
-const jsonParser = bodyParser.json();
+// const jsonParser = bodyParser.json();
 
 const router = new Router();
 module.exports = router;
 
 router.get('/list/:currencyId?', async (req: any, res: Response) => {
     const currencyId = req.params.currencyId ? req.params.currencyId : req.headers.clientcurrencyid;
-
-    console.log(currencyId);
     
     const vendorId = req.query.vendorId;
     const invoiceDateStart = req.query.invoiceDateStart;
@@ -42,11 +41,12 @@ router.get('/list/:currencyId?', async (req: any, res: Response) => {
 
     try {
         const { rows } = await db.query(`
-            SELECT "INVOICE_ID", "VENDOR_ID", "INVOICE_NUMBER", "INVOICE_TOTAL", "PAYMENT_TOTAL", "CREDIT_TOTAL", "BANK_ID"
+            SELECT "INVOICE_ID", "VENDOR_ID", "INVOICE_NUMBER", "INVOICE_TOTAL", "PAYMENT_TOTAL", "CREDIT_TOTAL", "BANK_ID", "CURRENCY"
                 FROM "INVOICES"${whereClause}
         `);
         // console.log(rows)
-        result = rows;
+        result = await convertCurrency(rows, parseInt(currencyId, 10));
+
         status = 200;
         message = OK_RESPONSE;
 
@@ -62,10 +62,64 @@ router.get('/list/:currencyId?', async (req: any, res: Response) => {
 });
 
 
-const formDateWhere = (start, end): string => {
+const formDateWhere = (start: string, end: string): string => {
     if (start && end) {
         return `"INVOICE_DATE" BETWEEN ${start} AND ${end}`;
     } else {
         return `"INVOICE_DATE" ${start ? '>=' : '<='} ${start ? start : end}`;
     }
+}
+
+
+const convertCurrency = async (data: Array<DbInvoice>, currencyId: number): Promise<any[]> => {
+    let convertedArr = [];
+
+    const { rows } = await db.query(`SELECT * FROM "currencies"`);
+    const currencies: any[] = rows;
+
+    const currencyToIndex = currencies.findIndex( x => x.id === currencyId );
+    const currencyTo = currencies.splice(currencyToIndex, 1)[0];
+
+    let converterUrl = `${process.env.C_CONV_URL}convert?q=`;
+
+    currencies.forEach(currency => {
+        converterUrl += `${currency.abbreviation}_${currencyTo.abbreviation},`;
+    });
+
+    converterUrl = converterUrl.substring(0, converterUrl.length - 1);
+    converterUrl += `&compact=ultra&apiKey=${process.env.C_CONV_KEY}`;
+
+    const convertionRate = await axios.get(converterUrl).then(response => response.data);
+    
+    data.forEach((invoice) => {
+        let convertedInvoice = {};
+
+        if(invoice.CURRENCY != currencyTo.abbreviation) {
+            convertedInvoice = {
+                invoiceId: invoice.INVOICE_ID,
+                vendorId: invoice.VENDOR_ID,
+                invoiceNumber: invoice.INVOICE_NUMBER,
+                invoiceTotal: parseFloat(invoice.INVOICE_TOTAL) * convertionRate[`${invoice.CURRENCY}_${currencyTo.abbreviation}`],
+                paymentTotal: parseFloat(invoice.PAYMENT_TOTAL) * convertionRate[`${invoice.CURRENCY}_${currencyTo.abbreviation}`],
+                creditTotal: parseFloat(invoice.CREDIT_TOTAL) * convertionRate[`${invoice.CURRENCY}_${currencyTo.abbreviation}`],
+                bankId: invoice.BANK_ID
+            };
+
+        } else {
+            convertedInvoice = {
+                invoiceId: invoice.INVOICE_ID,
+                vendorId: invoice.VENDOR_ID,
+                invoiceNumber: invoice.INVOICE_NUMBER,
+                invoiceTotal: parseFloat(invoice.INVOICE_TOTAL),
+                paymentTotal: parseFloat(invoice.PAYMENT_TOTAL),
+                creditTotal: parseFloat(invoice.CREDIT_TOTAL),
+                bankId: invoice.BANK_ID
+            };
+        }
+
+        convertedArr.push(convertedInvoice);
+
+    });
+
+    return convertedArr;
 }
---
+++ src/utils/interfaces/invoices.ts
@@ -0,0 +1,10 @@
+export interface DbInvoice {
+    INVOICE_ID: number,
+    VENDOR_ID: number,
+    INVOICE_NUMBER: string,
+    INVOICE_TOTAL: string,
+    PAYMENT_TOTAL: string,
+    CREDIT_TOTAL: string,
+    BANK_ID: number,
+    CURRENCY: string
+}
